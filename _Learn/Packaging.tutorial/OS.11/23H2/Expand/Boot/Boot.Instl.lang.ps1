19a5706205100785de10e831ce67370c10ba9178
iLikeYi <775159955@qq.com>
2025-10-16 21:42:21 +0800

    Latest

---
+++ _Learn/Packaging.tutorial/OS.11/23H2/Expand/Boot/Boot.Instl.lang.ps1
@@ -0,0 +1,46 @@
+﻿$Mount = "D:\OS_11_Custom\Boot\Boot\Mount"
+$Sources = "D:\OS_11_Custom\Boot\Boot\Language\Add\zh-CN"
+
+$Initl_install_Language_Component = @()
+Get-WindowsPackage -Path $Mount | ForEach-Object {
+    $Initl_install_Language_Component += $_.PackageName
+}
+
+Add-WindowsPackage -Path $Mount -PackagePath "$($Sources)\WinPE-FontSupport-zh-CN.cab"
+
+$Language = @(
+    @{ Match = "*WinPE*Setup*Client*Package*"; File = "WINPE-SETUP-CLIENT_zh-CN.CAB"; }
+    @{ Match = "*WinPE*Setup*Package*"; File = "WinPE-Setup_zh-CN.cab"; }
+    @{ Match = "*WinPE-LanguagePack-Package*"; File = "lp.cab"; }
+    @{ Match = "*SecureStartup*"; File = "winpe-securestartup_zh-CN.cab"; }
+    @{ Match = "*ATBroker*"; File = "winpe-atbroker_zh-CN.cab"; }
+    @{ Match = "*AudioCore*"; File = "winpe-audiocore_zh-CN.cab"; }
+    @{ Match = "*AudioDrivers*"; File = "winpe-audiodrivers_zh-CN.cab"; }
+    @{ Match = "*EnhancedStorage*"; File = "winpe-enhancedstorage_zh-CN.cab"; }
+    @{ Match = "*Narrator*"; File = "winpe-narrator_zh-CN.cab"; }
+    @{ Match = "*scripting*"; File = "winpe-scripting_zh-CN.cab"; }
+    @{ Match = "*Speech-TTS*"; File = "winpe-speech-tts_zh-CN.cab"; }
+    @{ Match = "*srh*"; File = "winpe-srh_zh-CN.cab"; }
+    @{ Match = "*srt*"; File = "winpe-srt_zh-CN.cab"; }
+    @{ Match = "*wds-tools*"; File = "winpe-wds-tools_zh-CN.cab"; }
+    @{ Match = "*-WMI-Package*"; File = "winpe-wmi_zh-CN.cab"; }
+)
+
+ForEach ($Rule in $Language) {
+    Write-Host "`n   Rule name: $($Rule.Match)" -ForegroundColor Yellow; Write-Host "   $('-' * 80)"
+    ForEach ($Component in $Initl_install_Language_Component) {
+        if ($Component -like "*$($Rule.Match)*") {
+            Write-Host "   Component name: " -NoNewline; Write-Host $Component -ForegroundColor Green
+            Write-Host "   Language pack file: " -NoNewline; Write-Host "$($Sources)\$($Rule.File)" -ForegroundColor Green
+            Write-Host "   Installing ".PadRight(22) -NoNewline
+            try {
+                Add-WindowsPackage -Path $Mount -PackagePath "$($Sources)\$($Rule.File)" | Out-Null
+                Write-Host "Finish" -ForegroundColor Green
+            } catch {
+                Write-Host "Failed" -ForegroundColor Red
+                Write-Host "   $($_)" -ForegroundColor Red
+            }
+            break
+        }
+    }
+}
